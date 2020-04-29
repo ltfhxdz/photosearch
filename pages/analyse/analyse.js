5896f15195c78ef6469f477329a21794d89c1fac
ltfhxdz <ltfhxdz@gmail.com>
2020-04-29 18:30:04 +0800

    完成身高、体重的显示

--- pages/analyse/analyse.js
+++ pages/analyse/analyse.js
@@ -4,24 +4,49 @@ const app = getApp()
 Page({
   data: {
     genderArray: ['女', '男'],
-    index: 0,
-    userInfo: {},
+    ageArray: [
+      [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150],
+      [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+    ],
+    heightArray: [
+      [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150, 160, 170, 180, 190, 200, 210, 220, 230, 240, 250],
+      [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+    ],
+    weightArray: [
+      [0, 100, 200, 300, 400, 500, 600, 700, 800, 900, 1000, 1100, 1200, 1300, 1400, 1500],
+      [0, 10, 20, 30, 40, 50, 60, 70, 80, 90],
+      [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+    ],
 
   },
 
-  ageMethod: function () {
-    console.log('enter age method');
+  heightMethod: function(e) {
+    let heightArray = e.detail.value;
+    let height = heightArray[0] * 10 + heightArray[1];
+    wx.setStorageSync('height', height);
+
+    this.setBasic();
+
+    this.setData({
+      height: wx.getStorageSync('height') + '厘米',
+      basic: wx.getStorageSync('basic') + '千卡'
+    })
   },
 
-  heightMethod: function () {
-    console.log('enter height method');
+  weightMethod: function(e) {
+    let weightArray = e.detail.value;
+    let weight = weightArray[0] * 100 + weightArray[1] * 10 + weightArray[2];
+    wx.setStorageSync('weight', weight);
+
+    this.setBasic();
+
+    this.setData({
+      weight: wx.getStorageSync('weight') + '斤',
+      basic: wx.getStorageSync('basic') + '千卡'
+    })
   },
 
-  weightMethod: function () {
-    console.log('enter weight method');
-  },
-
-  genderMethod: function (e) {
+  genderMethod: function(e) {
     if (e.detail.value == 0) {
       wx.setStorageSync('gender', '女');
     } else {
@@ -29,13 +54,13 @@ Page({
     }
     this.setBasic();
     this.setData({
-      index: e.detail.value,
+      genderIndex: e.detail.value,
       gender: wx.getStorageSync('gender'),
       basic: wx.getStorageSync('basic') + '千卡'
     })
   },
 
-  setBasic(){
+  setBasic() {
     let gender = wx.getStorageSync('gender');
     let age = wx.getStorageSync('age');
     let height = wx.getStorageSync('height');
@@ -51,9 +76,23 @@ Page({
     wx.setStorageSync('basic', basic);
   },
 
-  onLoad: function() {
 
-    if (wx.getStorageSync('gender') == ''){
+  ageMethod: function(e) {
+    let ageArray = e.detail.value;
+    let age = ageArray[0] * 10 + ageArray[1];
+    wx.setStorageSync('age', age);
+
+    this.setBasic();
+
+    this.setData({
+      age: wx.getStorageSync('age') + '岁',
+      basic: wx.getStorageSync('basic') + '千卡'
+    })
+  },
+
+  //设置默认值
+  setDefault() {
+    if (wx.getStorageSync('gender') == '') {
       wx.setStorageSync('gender', '男');
     }
     if (wx.getStorageSync('age') == '') {
@@ -65,17 +104,54 @@ Page({
     if (wx.getStorageSync('weight') == '') {
       wx.setStorageSync('weight', 171);
     }
-    
+  },
+
+  getGenderIndex() {
+    let genderIndex = 0;
+    if (wx.getStorageSync('gender') == '男') {
+      genderIndex = 1;
+    } else {
+      genderIndex = 0;
+    }
+    return genderIndex;
+  },
+
+  getAgeIndex() {
+    let age = wx.getStorageSync('age');
+    let a = parseInt(age / 10);
+    let b = age % 10;
+    let ageIndex = [a, b];
+    return ageIndex;
+  },
+
+  getHeightIndex() {
+    let height = wx.getStorageSync('height');
+    let a = parseInt(height / 10);
+    let b = height % 10;
+    let heightIndex = [a, b];
+    return heightIndex;
+  },
+
+  getweightIndex() {
+    let weight = wx.getStorageSync('weight');
+    let a = parseInt(weight / 100);
+    let b = parseInt(weight % 100 / 10);
+    let c = weight % 10;
+    let weightIndex = [a, b, c];
+    return weightIndex;
+  },
+
+  onLoad: function() {
+    //设置默认值
+    this.setDefault();
+    //设置基础代谢
     this.setBasic();
 
-    let index = 0;
-    if (wx.getStorageSync('gender') == '男'){
-      index = 1;
-    }else{
-      index = 0;
-    }
     this.setData({
-      index: index,
+      genderIndex: this.getGenderIndex(),
+      ageIndex: this.getAgeIndex(),
+      heightIndex: this.getHeightIndex(),
+      weightIndex: this.getweightIndex(),
       gender: wx.getStorageSync('gender'),
       age: wx.getStorageSync('age') + '岁',
       height: wx.getStorageSync('height') + '厘米',
